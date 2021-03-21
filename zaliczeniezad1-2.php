97c4ab7ba0c1239811c728659536720af6720e28
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2021-03-21 20:48:46 +0100

    Update zaliczeniezad1-2.php

--- zaliczeniezad1-2.php
+++ zaliczeniezad1-2.php
@@ -2,17 +2,13 @@
 <html lang="pl">
 
   <head>
-
      <meta charset="utf-8">
      <title>Daniel zadanie 1-2</title>
-
      <meta name="description" content="php">
      <meta name="keywords" content="php zadania 1-2">
      <meta name="Daniel" content="php">
-
      <meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
      <link rel="stylesheet" href="arkusz.css">
-
   </head>
   <body>
   
