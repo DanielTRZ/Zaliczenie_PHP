e8981be3bf8472bda3fdda68bc00b3a0dbc3013a
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2021-03-21 20:47:20 +0100

    Update zaliczeniezad8.php

--- zaliczeniezad8.php
+++ zaliczeniezad8.php
@@ -2,17 +2,14 @@
 <html lang="pl">
 
   <head>
-
      <meta charset="utf-8">
      <title>Daniel zadanie 8</title>
 
      <meta name="description" content="php">
      <meta name="keywords" content="php zadania8">
      <meta name="Daniel" content="php">
-
      <meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
      <link rel="stylesheet" href="arkusz.css">
-
   </head>
   <body>
   
@@ -34,5 +31,4 @@
 ?>
 
   </body>
-
 </html>
