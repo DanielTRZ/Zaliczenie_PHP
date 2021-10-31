16587ddf9353606a0f25f565c20aa6d9494350a5
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2021-10-31 16:22:57 +0100

    Update zaliczeniezad8.php

--- zaliczeniezad8.php
+++ zaliczeniezad8.php
@@ -4,7 +4,6 @@
   <head>
      <meta charset="utf-8">
      <title>Daniel zadanie 8</title>
-
      <meta name="description" content="php">
      <meta name="keywords" content="php zadania8">
      <meta name="Daniel" content="php">
@@ -12,17 +11,16 @@
      <link rel="stylesheet" href="arkusz.css">
   </head>
   <body>
-  
-   
-<?php
-  if (!file_exists('sedziowie.txt')) 
-  {
-    echo 'Nie znaleziono pliku!';
-    exit;
-  }
-  $tablica=file('sedziowie.txt');
-  if (count($tablica)===0)
-  {
+     
+          <?php
+            if (!file_exists('sedziowie.txt')) 
+           {
+            echo 'Nie znaleziono pliku!';
+            exit;
+           }
+            $tablica=file('sedziowie.txt');
+            if (count($tablica)===0)
+           {
     echo 'Plik jest pusty!';
     exit;
   }
