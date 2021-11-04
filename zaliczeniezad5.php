0822ad3a667f9867d46423f20840c7c2f6bbb689
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2021-11-04 09:42:49 +0100

    Update zaliczeniezad5.php

--- zaliczeniezad5.php
+++ zaliczeniezad5.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="pl">
-
   <head>
      <meta charset="utf-8">
      <title>Daniel zadanie5</title>
@@ -10,7 +9,7 @@
      <meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
      <link rel="stylesheet" href="arkusz.css">
   </head>
-  <body>
+ <body>
   
   <?php
       
