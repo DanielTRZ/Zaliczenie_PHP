ccd7e7dc26daf0788158d69fd998aa2702e4affc
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-10-02 18:38:24 +0200

    Update liczby.php

--- liczby.php
+++ liczby.php
@@ -19,7 +19,7 @@ Liczba Końcowa <input type=numeric name="Stop"/><br/>
 <br>
 <input type=submit value="Wyślij"/>
 </form>
-  <br>
+<br>
   <br>
   Start: <?php echo $_GET['Start']?><br>
   Stop: <?php echo $_GET['Stop']?><br>
