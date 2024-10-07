d56746c7ade0ecc7923f165448ff5bcdbb2aada5
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-10-07 10:24:59 +0200

    Update zaliczeniezad1-2.php

--- zaliczeniezad1-2.php
+++ zaliczeniezad1-2.php
@@ -39,7 +39,7 @@ $b2=8;
 $c3=6;
 if ($a1 < $b2 || $b< $c3){
 echo "<br>";
-        echo "$a1 jest mniejsze $b2 a $b2 jest wieksza od $c3";
+echo "$a1 jest mniejsze $b2 a $b2 jest wieksza od $c3";
         echo"<br>";
         echo "Największa liczbą jest $b2";
 
