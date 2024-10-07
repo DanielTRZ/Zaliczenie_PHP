06b00efa7781509e485ec7afb3a5d185b4696b51
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-10-07 10:21:51 +0200

    Update zaliczeniezad1-2.php

--- zaliczeniezad1-2.php
+++ zaliczeniezad1-2.php
@@ -26,7 +26,7 @@ echo "<br>";
 if($numer===$numer2){
 echo " $numer jest tego samego typu $numer2";}
 else{
-        echo "$numer nie jest tego samego typu  $numer2";}
+echo "$numer nie jest tego samego typu  $numer2";}
         echo"<br>"; 
             
       // operator ==	Prawda jeśli $a jest równe $b.
