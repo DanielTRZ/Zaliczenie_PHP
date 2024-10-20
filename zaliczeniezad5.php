4074351eed15eacc28b746e68c31a9406c7d6357
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-10-20 04:07:10 +0200

    Update zaliczeniezad5.php

--- zaliczeniezad5.php
+++ zaliczeniezad5.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <title>Daniel zadanie5</title>
      <meta name="description" content="php">
-     <meta name="keywords" content="zadanie 3,4">
+<meta name="keywords" content="zadanie 3,4">
      <meta name="Daniel" content="php">
      <meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
      <link rel="stylesheet" href="arkusz.css">
